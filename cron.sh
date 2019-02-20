f72a61896aea942c221b06ac81f46d83da8eb9b3
macbre <maciej.brencz@gmail.com>
2019-02-20 18:10:43 +0100

    Fix cron.sh

--- cron.sh
+++ cron.sh
@@ -3,7 +3,8 @@ date
 source env/bin/activate
 make
 git diff
-git add docs/ && git commit -m 'Updating the Planet' && git push origin master
+git add docs/ && git commit -m 'Updating the Planet'
+git push origin master
 
 echo 'Done'
 date
